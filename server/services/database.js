2774295550220696a30e6f58e429e6e9e58eb569
eva <evapamfil1@gmail.com>
2018-07-12 00:11:20 +0200

    mysql

--- server/services/database.js
+++ server/services/database.js
@@ -1,7 +1,7 @@
 var mysql = require('mysql');
 
 var connection = mysql.createConnection({
-    host: '127.0.0.1',
+    host: '18.218.6.100',
     user: 'eva',
     password: 'eva',
     database: 'Popeat'
